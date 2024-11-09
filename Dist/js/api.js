a3deb7ab4abac243296589b718d70c488d39b8b9
Pasha <pashamalik9371@gmail.com>
2024-11-10 03:23:24 +0700

    fix deploy v1

--- Dist/js/api.js
+++ Dist/js/api.js
@@ -2,13 +2,16 @@ async function fetchRepos() {
   try {
     const reposContainer = document.getElementById('repos');
     const fragment = document.createDocumentFragment();
-    
+
+    // Fetch data dari API GitHub
     const response = await fetch('https://api.github.com/users/npma7/repos');
     const repos = await response.json();
 
+    // Filter repositori yang tidak forked
     const nonForkedRepos = repos.filter(repo => !repo.fork);
-    
-    const createRepoCard = ({name, description, stargazers_count, forks_count, language, html_url}) => {
+
+    // Fungsi untuk membuat card repo
+    const createRepoCard = ({ name, description, stargazers_count, forks_count, language, html_url }) => {
       const card = document.createElement('div');
       card.className = 'repo-data';
       card.innerHTML = `
@@ -24,15 +27,17 @@ async function fetchRepos() {
       return card;
     };
 
+    // Buat card untuk setiap repo
     nonForkedRepos.forEach(repo => {
       fragment.appendChild(createRepoCard(repo));
     });
 
     reposContainer.appendChild(fragment);
-    
+
   } catch (error) {
     console.error('Error fetching repos:', error);
   }
 }
 
-fetchRepos();
+// Panggil fungsi fetchRepos setelah DOM siap
+document.addEventListener('DOMContentLoaded', fetchRepos);
